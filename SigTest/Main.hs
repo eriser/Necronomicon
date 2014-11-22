82531aac6f5d8a5a070b25d5b5d65466e939582a
CurtisMcKinney <casiosk1@gmail.com>
2014-11-21 23:27:15 -0500

    added dictionaried threads to prevent needless thread spawning

--- SigTest/Main.hs
+++ SigTest/Main.hs
@@ -1,9 +1,8 @@
-module Main where
-
+import Prelude
 import Necronomicon.FRP
 
 main :: IO()
--- main = runSignal tonsOfMouseAndTime
+main = runSignal tonsOfMouseAndTime
 -- main = runSignal $ dropIf (\(x,y) -> x > 400) (0,0) mousePos
 -- main = runSignal $ keepIf (\(x,y) -> x > 400) (0,0) mousePos
 -- main = runSignal $ sampleOn mouseClicks mousePos
@@ -11,19 +10,21 @@ main :: IO()
 -- main = runSignal $ every $ 2 * second
 -- main = runSignal $ dropWhen ((\(x,_) -> x > 400) <~ mousePos) mousePos
 -- main = runSignal $ sampleOn mouseClicks doubleMouse <|> mousePos
-main = runSignal $ every second <|> fps 4.5
+-- main = runSignal $ every second <|> fps 4.5
 
 doubleMouse :: Signal (Double,Double)
 doubleMouse = (\(x,y) -> (x*2,y*y)) <~ mousePos
 
 tonsOfMouseAndTime :: Signal (Double,Double)
-tonsOfMouseAndTime = hundredTests
+tonsOfMouseAndTime = thousandsTests
     where
         tupleTest x _ = x
-        tupleTime               = (\x ->  (x,x)) <~ fps 30
+        tupleTime               = (\x ->  (x,x)) <~ every second
         test                    = tupleTest <~ mousePos
-        tenTests                = test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~(test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ tupleTime))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))
+        tenTests                = test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ (test ~~ mousePos)))))))))
         test2                   = tupleTest <~ tenTests
         hundredTests            = test2 ~~ (test2 ~~ (test2 ~~ (test2 ~~ (test2 ~~ (test2 ~~ (test2 ~~ (test2 ~~ (test2 ~~ (test2 ~~ mousePos)))))))))
-
-
+        test3                   = tupleTest <~ hundredTests
+        thousandsTests          = test3 ~~ (test3 ~~ (test3 ~~ (test3 ~~ (test3 ~~ (test3 ~~ (test3 ~~ (test3 ~~ (test3 ~~ (test3 ~~ mousePos)))))))))
+        test4                   = tupleTest <~ thousandsTests
+        tenThousandTest         = test4 ~~ (test4 ~~ (test4 ~~ (test4 ~~ mousePos)))
