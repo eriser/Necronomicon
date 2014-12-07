fd9c3fc5c8a7041140af65a3c7b65a118c707916
CurtisMcKinney <casiosk1@gmail.com>
2014-12-06 22:59:20 -0500

    added noise modules

---
+++ src/Necronomicon/Noise.hs
@@ -0,0 +1,6 @@
+module Necronomicon.Noise (module Necronomicon.Noise.Simplex,
+                           module Necronomicon.Noise.DiamondSquare) where
+
+import Necronomicon.Noise.Simplex
+import Necronomicon.Noise.DiamondSquare
+
