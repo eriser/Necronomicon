0fed10a26908f3d6821e085cde956164dc2cf1ad
CurtisMcKinney <casiosk1@gmail.com>
2014-12-10 08:45:45 -0500

    generalizing mesh types

--- src/Necronomicon/Graphics/Mesh.hs
+++ src/Necronomicon/Graphics/Mesh.hs
@@ -4,17 +4,35 @@ import Prelude
 import Necronomicon.Linear
 import Necronomicon.Graphics.Color
 
-data Mesh = Mesh {
-    vertices  :: [Vector3],
-    colors    :: [Color]
-} deriving (Show)
+class MeshType a where
+    vertices :: a -> [Vector3]
+    colors   :: a -> [Color]
+
+data Mesh = Mesh [Vector3] [Color] deriving (Show)
+
+instance MeshType Mesh where
+    vertices (Mesh v _) = v
+    colors   (Mesh _ c) = c
+
+data SimpleMesh = SimpleMesh {
+    _simpleVertices  :: [Vector3],
+    _simpleColors    :: [Color]
+    } deriving (Show)
+
+instance MeshType SimpleMesh where
+    vertices (SimpleMesh v _) = v
+    colors   (SimpleMesh _ c) = c
 
 data Mesh' = Mesh' {
     _vertices :: [Vector3],
-    _normals  :: [Vector3],
-    _tangents :: [Vector3],
-    _uvs      :: [Vector2],
     _colors   :: [Color],
+    _normals  :: Maybe [Vector3],
+    _tangents :: Maybe [Vector3],
+    _uvs      :: Maybe [Vector2],
     _indices  :: [Int]
     } deriving (Show)
+
+instance MeshType Mesh' where
+    vertices (Mesh' v _ _ _ _ _) = v
+    colors   (Mesh' _ c _ _ _ _) = c
 
