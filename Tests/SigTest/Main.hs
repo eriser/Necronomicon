cdb2a0183c291acf00471117073bc7efbba8c2d7
CurtisMcKinney <casiosk1@gmail.com>
2015-02-19 19:26:26 -0500

    cleaning up

--- Tests/SigTest/Main.hs
+++ Tests/SigTest/Main.hs
@@ -82,13 +82,14 @@ threeSynth :: UGen -> UGen -> UGen -> UGen
 threeSynth fx fy fz = sin fx + sin fy + sin fz |> gain 0.1 |> out 0
 
 testScene :: Signal ()
--- testScene = scene [pure cam,terrainSig]
-testScene = scene [pure cam,oscillatorObject]
+testScene = scene [pure cam,oscSig]
     where
         move (x,y) z a = Vector3 (x*z*a) (y*z*a) 0
         cam            = perspCamera (Vector3 0 0 10) identity 60 0.1 1000 black [glow]
         terrain pos    = SceneObject pos identity 1 (Model simplexMesh $ vertexColored (RGBA 1 1 1 0.35)) []
         terrainSig     = terrain <~ (lagSig 4 $ foldn (+) 0 (lift3 move arrows (fps 4) 3))
+        oscSig         = oscillatorObject <~ audioBuffer 2 ~~ audioBuffer 3 ~~ audioBuffer 4
+
         -- terrainSig     = terrain <~ playPattern 0 (isDown keyP) (isDown keyP)
             -- [lich| [0 1 2 3] [4 5 6 7] [6 0 5 0] [4 0 3 0] |]
 
@@ -115,14 +116,10 @@ simplexMesh = Mesh "simplex" vertices colors uvs indices
         uvs      = map toUV     values
         indices  = foldr (addIndices (round w)) [] [0..(length values)]
 
-oscillatorObject :: Signal SceneObject
-oscillatorObject = model <~ (oscillatorMesh <~ audioBuffer 2 ~~ audioBuffer 3 ~~ audioBuffer 4)
+oscillatorObject :: [Double] -> [Double] -> [Double] -> SceneObject
+oscillatorObject audioBuffer1 audioBuffer2 audioBuffer3 = SceneObject 0 identity 1 (Model mesh $ vertexColored (RGBA 1 1 1 0.35)) []
     where
-        model mesh = SceneObject 0 identity 1 (Model mesh $ vertexColored (RGBA 1 1 1 0.35)) []
-
-oscillatorMesh :: [Double] -> [Double] -> [Double] -> Mesh
-oscillatorMesh audioBuffer1 audioBuffer2 audioBuffer3 = DynamicMesh "osc1" vertices colors uvs indices
-    where
+        mesh                                         = DynamicMesh "osc1" vertices colors uvs indices
         scale                                        = 6
         width                                        = 1
         indices                                      = foldr (\i acc -> i + 1 : i + 2 : i + 3 : i + 1 : i + 0 : i + 2 : acc) [] [0..511]
@@ -148,48 +145,6 @@ oscillatorMesh audioBuffer1 audioBuffer2 audioBuffer3 = DynamicMesh "osc1" verti
                 r1  = vtoc (np1 * 0.5 + 0.5) 0.35
                 r2  = vtoc (np2 * 0.5 + 0.5) 0.35
                 r3  = vtoc (np3 * 0.5 + 0.5) 0.35
-
-{-
-oscillatorMesh :: Signal Mesh
-oscillatorMesh = Signal $ \state -> do
-    (vertexBuffer,indexBuffer) <- genDynMeshBuffers
-    audioCont0                 <- unSignal (audioBuffer 2) state
-    audioCont1                 <- unSignal (audioBuffer 3) state
-    audioCont2                 <- unSignal (audioBuffer 4) state
-    return $ processSignal vertexBuffer indexBuffer audioCont0 audioCont1 audioCont2
-    where
-        processSignal vertexBuffer indexBuffer audioCont0 audioCont1 audioCont2 state = do
-            audio0 <- fmap unEvent $ audioCont0 state
-            audio1 <- fmap unEvent $ audioCont1 state
-            audio2 <- fmap unEvent $ audioCont1 state
-            let zippedAudio       = zip3 audio0 audio1 audio2
-                (vertices,colors) = foldr toVertex ([],[]) $ zip zippedAudio $ drop 1 zippedAudio
-            return $ Change $ DynamicMesh vertexBuffer indexBuffer vertices colors uvs indices
-
-        -- colors  = replicate 511 white
-        uvs     = replicate 512 0
-        indices = foldr (\i acc -> i + 1 : i + 2 : i + 3 : i + 1 : i + 0 : i + 2 : acc ) [] [0..511]
-        scale   = 6
-        width   = 1
-        toVertex ((x1,y1,z1),(x2,y2,z2)) (vacc,cacc) = (p3 : p2 : p1 : p0 : vacc,r3 : r2 : r1 : r0 : cacc)
-            where
-                cp = cross np0 np1
-
-                p0 = Vector3 (x1 * scale) (y1 * scale) (z1 * scale * 0.35)
-                p1 = Vector3 (x2 * scale) (y2 * scale) (z2 * scale * 0.35)
-                p2 = p0 + cp * width
-                p3 = p1 + cp * width
-
-                np0 = normalize p0
-                np1 = normalize p1
-                np2 = normalize p2
-                np3 = normalize p3
-
-                r0 = vtoc (np0 * 0.5 + 0.5) 0.35
-                r1 = vtoc (np1 * 0.5 + 0.5) 0.35
-                r2 = vtoc (np2 * 0.5 + 0.5) 0.35
-                r3 = vtoc (np3 * 0.5 + 0.5) 0.35
--}
 
 triOsc :: UGen -> UGen -> [UGen]
 triOsc f1 f2 = [sig1,sig2] + [sig3,sig3] |> verb |> gain 0.1 |> out 0
