f242402d1abb8b94cfbedfbd9231c1e42ae5f0be
Chad McKinney <chad@chadmckinneyaudio.com>
2015-05-08 15:50:10 -0700

    Added multicolored wolfram CA

--- Tests/GenerativeTest/Main.hs
+++ Tests/GenerativeTest/Main.hs
@@ -3,7 +3,7 @@ import Control.Monad
 import qualified Data.Vector as V
 
 main :: IO ()
-main = testLSystem >> testWolfram
+main = testLSystem >> testWolfram >> testMultiColoredWolfram
 
 testLSystem :: IO ()
 testLSystem = mapM_ (\(name, gen) -> putStrLn (formatName name) >> (mapM_ (print . gen) iterations) >> (putStrLn "\n")) lsystems
@@ -27,7 +27,7 @@ testWolfram = do
     print "--------------------------------------------------------------------------------------------------------------------"
     print "-- Wolfram"
     print "--------------------------------------------------------------------------------------------------------------------"
-    mapM_ (\n -> printWolframRule n >> putStrLn "\n") ([0..255] :: [Int])
+    mapM_ (\n -> printWolframRule n >> putStrLn "\n") ([18, 30, 73, 103, 105, 110, 150, 169, 193, 197] :: [Int])
     where
         wolframCellToAscii cell = case cell of
             White -> ' '
@@ -43,4 +43,27 @@ testWolfram = do
                 printAndfoldWolfram cells _ = do
                     let cells' = wolframCA cells ruleVector
                     print $ V.map wolframCellToAscii cells'
+                    return cells'
+
+testMultiColoredWolfram :: IO ()
+testMultiColoredWolfram = do
+    print "--------------------------------------------------------------------------------------------------------------------"
+    print "-- Multi-Colored Wolfram"
+    print "--------------------------------------------------------------------------------------------------------------------"
+    mapM_ (\n -> printWolframRule n >> putStrLn "\n") ([77245, 210819, 325311, 497049, 573377, 706951, 974099] :: [Int])
+    where
+        numColors = 3
+        toAscii :: Int -> Char
+        toAscii n = toEnum (n + 93)
+        startingRow = V.fromList (replicate 80 0 ++ [1] ++ replicate 80 0)
+        startingRowAscii = V.map toAscii startingRow
+        printWolframRule rule = do
+            print $ "Number of Colors: " ++ show numColors ++ " Rule: " ++ show rule
+            print startingRowAscii
+            foldM_ printAndfoldWolfram startingRow ([0..50] :: [Int])
+            where
+                ruleMap = multiColoredWolframRuleMap numColors rule
+                printAndfoldWolfram cells _ = do
+                    let cells' = multiColoredWolframCA cells ruleMap
+                    print $ V.map toAscii cells'
                     return cells'
--- src/Necronomicon/Noise/Wolfram.hs
+++ src/Necronomicon/Noise/Wolfram.hs
@@ -2,29 +2,70 @@ module Necronomicon.Noise.Wolfram where
 
 import Data.Bits
 import qualified Data.Vector as V
+import qualified Data.List as L
+import qualified Data.Set as S
+import qualified Data.Map as M
+import Control.Monad
 
 data WolframCell = White | Black deriving (Ord, Eq, Enum, Show)
 
+-- Creates a rule vector to be used with wolframCA. The rule number is an integer from 0..255.
 binaryWolframRuleVector :: Int -> V.Vector WolframCell
-binaryWolframRuleVector ruleSet = V.fromList $ foldr foldRule [] [max8BitIndex,  (max8BitIndex - 1) ..  min8BitIndex]
+binaryWolframRuleVector ruleNumber = V.fromList $ foldr foldRule [] [max8BitIndex,  (max8BitIndex - 1) ..  min8BitIndex]
     where
         min8BitValue = 0
         max8BitValue = 255
         min8BitIndex = 0
         max8BitIndex = 7
-        ruleSet' = min max8BitValue (max min8BitValue ruleSet)
+        ruleNumber' = min max8BitValue (max min8BitValue ruleNumber)
         mask = 1
-        foldRule i acc = toEnum (ruleSet' `shiftR` (max8BitIndex - i) .&. mask) : acc
+        foldRule i acc = toEnum (ruleNumber' `shiftR` (max8BitIndex - i) .&. mask) : acc
 
+-- Elementary Cellular Automata, as defined by Wolfram. requires a rule vector created by binaryWolframRuleVector to be passed in
 wolframCA :: V.Vector WolframCell -> V.Vector WolframCell -> V.Vector WolframCell
-wolframCA cells ruleList = V.fromList $ foldr binaryWolfram [] [0..maxCellIndex]
+wolframCA cells ruleVector = V.fromList $ foldr binaryWolfram [] [0..maxCellIndex]
     where
         cellsLength = length cells
         maxCellIndex = max 0 (cellsLength - 1)
-        ruleListLength = length ruleList
-        binaryWolfram i acc = (ruleList V.! rule) : acc
+        ruleVectorLength = length ruleVector
+        defaultBlankCell = White
+        indexFunc left middle right = (fromEnum left `shiftL` 2) + (fromEnum middle `shiftL` 1) + fromEnum right
+        binaryWolfram i acc = (ruleVector V.! rule) : acc
             where
-                left = if leftIndex >= 0 then cells V.! leftIndex else White where leftIndex = i - 1
+                left = if leftIndex >= 0 then cells V.! leftIndex else defaultBlankCell where leftIndex = i - 1
                 middle = cells V.! i
-                right = if rightIndex < cellsLength then cells V.! rightIndex else White where rightIndex = i + 1
-                rule = min ruleListLength (max 0 ((fromEnum left `shiftL` 2) + (fromEnum middle `shiftL` 1) + fromEnum right))
+                right = if rightIndex < cellsLength then cells V.! rightIndex else defaultBlankCell where rightIndex = i + 1
+                rule = min ruleVectorLength (max 0 (indexFunc left middle right))
+
+-- Creates a rule map to be used with multiColoredWolframCA. Colors are Integers from 0 .. (numColors - 1). The rule can be any positive integer.
+multiColoredWolframRuleMap :: Int -> Int -> M.Map Double Int
+multiColoredWolframRuleMap numColors ruleNumber = foldl ruleNumberAndAverageToColor M.empty $ zip uniqueAveragesList [0 .. lastUniqueAveragesIndex]
+    where
+        uniqueAverages = foldl (\acc xs -> S.insert ((/numColorsDouble) $ sum xs) acc) S.empty $ replicateM numNeighbors [0.0 .. maxColorIndex]
+        uniqueAveragesList = L.sort $ S.toList uniqueAverages
+        lastUniqueAveragesIndex = max 0 $ (length uniqueAveragesList) - 1
+        ruleNumberDouble = fromIntegral (max 0 ruleNumber) :: Double
+        numColorsDouble = fromIntegral numColors :: Double
+        numNeighbors = 3
+        maxColorIndex = fromIntegral . max 0 $ numColors - 1
+        ruleNumberAndAverageToColor acc (average, i) = M.insert average color acc
+            where
+                color = mod (floor (ruleNumberDouble / (fromIntegral (numColors ^ i)))) numColors
+
+-- Multicolored Wolframa Cellular Automata. Requires a rule map created with multiColoredWolframRuleMap to be passed in.
+multiColoredWolframCA :: V.Vector Int -> M.Map Double Int -> V.Vector Int
+multiColoredWolframCA cells ruleMap = V.fromList $ foldr multiColoredWolfram [] [0..maxCellIndex]
+    where
+        numNeighbors = 3 :: Double
+        defaultBlankCell = 0 :: Int
+        cellsLength = length cells
+        maxCellIndex = max 0 (cellsLength - 1)
+        multiColoredWolfram i acc = color : acc
+            where
+                left = if leftIndex >= 0 then cells V.! leftIndex else defaultBlankCell where leftIndex = i - 1
+                middle = cells V.! i
+                right = if rightIndex < cellsLength then cells V.! rightIndex else defaultBlankCell where rightIndex = i + 1
+                rule = fromIntegral (left + middle + right) / numNeighbors
+                color = case (M.lookup rule ruleMap) of
+                    Nothing -> defaultBlankCell
+                    Just c -> c
