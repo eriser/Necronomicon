f63707765a8dd548de1133cacd18285b66cb4665
CurtisMcKinney <casiosk1@gmail.com>
2015-09-09 15:49:48 -0400

    Networking tweaks

--- src/Necronomicon/Networking/Message.hs
+++ src/Necronomicon/Networking/Message.hs
@@ -10,7 +10,7 @@ import Data.Binary                            (encode, decode)
 import Data.Word                              (Word16)
 import Control.Exception
 import qualified Data.ByteString.Lazy  as BL
-import qualified Data.ByteString as B
+-- import qualified Data.ByteString as B
 
 data Receive = Receive BL.ByteString
              | ShutdownMessage
@@ -32,7 +32,7 @@ sendWithLength nsocket msg = Control.Exception.catch trySend onFailure
             | otherwise = do
                 let messageLengthData = encode messageLength
                 -- putStrLn $ "length of messageLength: " ++ show (B.length messageLengthData)
-                -- putStrLn $ "sending message of length: " ++ show messageLength
+                putStrLn $ "sending message of length: " ++ show messageLength
                 sendAll nsocket $ BL.toStrict messageLengthData
                 sendAll nsocket $ BL.toStrict msg
         onFailure e = print (e :: IOException)
@@ -41,24 +41,26 @@ receiveWithLength :: Socket -> IO Receive
 receiveWithLength nsocket = Control.Exception.catch trySend onFailure
     where
         onFailure e = return $ Exception e
-        trySend     = isConnected nsocket >>= \connected -> if not connected then return ShutdownMessage else recv nsocket lengthOfMessageLength >>= \len -> case decodeTransLength len of
+        trySend     = isConnected nsocket >>= \connected -> if not connected then return ShutdownMessage else readTillFinished (fromIntegral lengthOfMessageLength) BL.empty >>= \len -> case decodeTransLength len of
             Nothing   -> return IncorrectLength
             Just len' -> if len' <= 0
                 then return ShutdownMessage
-                else readTillFinished len' BL.empty
+                else readTillFinished len' BL.empty >>= \msg -> if BL.null msg
+                    then return ShutdownMessage
+                    else return $ Receive msg
 
         readTillFinished amountToRead prevData = do
-            -- putStrLn $ "Attempting to receive data of length: " ++ show amountToRead
+            putStrLn $ "Attempting to receive data of length: " ++ show amountToRead
             streamData <- BL.fromStrict <$> recv nsocket amountToRead
-            -- putStrLn $ "Actually received data of length: " ++ show (B.length streamData)
+            putStrLn $ "Actually received data of length: " ++ show (BL.length streamData)
             -- putStrLn ""
-            if BL.length streamData <= 0 then return ShutdownMessage else if fromIntegral (BL.length streamData) < amountToRead
-                then {-putStrLn "continue receiving" >> -} (readTillFinished (amountToRead - fromIntegral (BL.length streamData)) $ BL.append prevData streamData)
-                else {-putStrLn "done receiving" >> -} (return $ Receive $ BL.append prevData streamData)
+            if BL.length streamData <= 0 then return BL.empty else if fromIntegral (BL.length streamData) < amountToRead
+                then readTillFinished (amountToRead - fromIntegral (BL.length streamData)) $ BL.append prevData streamData
+                else return $ BL.append prevData streamData
 
 
         decodeTransLength bs
-            | B.length bs == 0                     = Just 0
-            | B.length bs == lengthOfMessageLength = Just $ fromIntegral (decode (BL.fromStrict bs) :: Word16)
-            | otherwise                            = Nothing
+            | BL.null bs                                           = Just 0
+            | (fromIntegral (BL.length bs) :: Int)  == lengthOfMessageLength = Just $ fromIntegral (decode bs :: Word16)
+            | otherwise                                            = Nothing
 
